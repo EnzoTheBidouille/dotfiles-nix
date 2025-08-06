aeb99b23c536002edc8d0d662e85ec50cde9778c
Enzo Viry <46194336+EnzoTheBidouille@users.noreply.github.com>
2025-08-06 14:53:55 +0000

    update home path

--- home/enzo/home.nix
+++ home/enzo/home.nix
@@ -6,9 +6,9 @@
   programs.zsh.enable = true;
 
   home.file = {
-    ".config/hypr/hyprland.conf".source = ../dotfiles/hypr/hyprland.conf;
-    ".config/waybar/config".source = ../dotfiles/waybar/config;
-    ".config/waybar/style.css".source = ../dotfiles/waybar/style.css;
+    ".config/hypr/hyprland.conf".source = ../../dotfiles/hypr/hyprland.conf;
+    ".config/waybar/config".source = ../../dotfiles/waybar/config;
+    ".config/waybar/style.css".source = ../../dotfiles/waybar/style.css;
   };
 
   home.stateVersion = "25.05";
